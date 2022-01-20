3f3a261fd5fce6f4ab1c82c87ff833c123ea841b
FloarKycyku <fk47206@ubt-uni.net>
2022-01-20 18:24:02 +0100

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import com.company.model.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Product product = new Product (name: "Keyboard" );
+
+        System.out.println(product.getName());
+     }
+}
